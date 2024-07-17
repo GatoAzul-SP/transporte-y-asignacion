e07e21791f1919ac89e6e72aeaf3f6b3d59c2310
CATZ <13862383+CmdteAlphaTigreZ@users.noreply.github.com>
2024-07-17 16:34:32 -0400

    Arreglar matriz_costos en función asignacion en cli.py para que esté rellena con ceros faltantes

--- cli.py
+++ cli.py
@@ -73,6 +73,7 @@ def asignacion():
         funcion_estado = inicio
         return
 
+    matriz_costos = mh.procesar_matriz(matriz_costos)
     salida = [["Centro", "Ruta"]]
     salida += ([str(par[0] + 1), str(par[1] + 1)] for par in asignaciones)
     salida += [["Total", str(sum(matriz_costos[i][j] for i, j in asignaciones))]]
