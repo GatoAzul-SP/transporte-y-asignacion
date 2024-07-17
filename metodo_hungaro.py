f674d0e25c26ba6c105ab7582f2cb715d122f34f
CATZ <13862383+CmdteAlphaTigreZ@users.noreply.github.com>
2024-07-17 01:11:16 -0400

    Adición de función de pasos, ajuste en procesar_matriz y pequeños arreglos

--- metodo_hungaro.py
+++ metodo_hungaro.py
@@ -1,52 +1,112 @@
 # Método húngaro
 # Autor: Andrés Gallegos y Santiago Pinto                Fecha: 2024-07-17
+
 INFINITO = float("inf")
 
-def hungaro(matriz_costos, disponibilidad_uniforme=True):
+PASO_PROCESAR = 1
+PASO_REDUCCION_FILAS = 2
+PASO_REDUCCION_COLUMNAS = 3
+PASO_ASIGNACION_INICIAL = 4
+PASO_INICIO_MARCADO = 5
+PASO_FIN_MARCADO = 6
+PASO_ASIGNACION_FINAL = 7
+
+def hungaro(matriz_costos, funcion_pasos=None, disponibilidad_uniforme=True):
+    """Calcula la asignación óptima con el método húngaro.
+
+    La función acepta matrices de costo cuadradas y rectangulares; en el último
+    caso, completa las filas o columnas faltantes con ceros.
+
+    'funcion_pasos' es una función que debe tener estos parámetros
+    en orden: matriz de costos, matriz de ceros marcados,
+    vector de filas marcadas, vector de columnas marcadas,
+    asignaciones, código de paso.
+    El código de paso corresponde a las constantes PASO_* definidas
+    en el módulo.
+
+    PASO_PROCESAR ocurre luego de validar, copiar y procesar la matriz de costos.
+        Sólo la matriz de costos no es None
+    PASO_REDUCCION_FILAS ocurre luego de restar los menores de cada fila a sus
+        filas respectivas.
+    PASO_REDUCCION_COLUMNAS ocurre de manera similar que el anterior, pero con
+        columnas.
+    PASO_ASIGNACION_INICIAL ocurre luego de la búsqueda de la asignación
+        inicial, que podría coincidir con la final si satisface las condiciones;
+        en este caso, no ocurriría ningún paso siguiente.
+        Desde este paso se da el argumento de asignaciones.
+    PASO_INICIO_MARCADO ocurre varias veces, antes del subalgoritmo de marcado
+        que busca la posibilidad de realizar una reasignación, y luego de
+        la primera iteración coincide luego de la ejecución de los pasos
+        restantes excepto el final.
+        Desde este paso se dan todos los argumentos.
+    PASO_FIN_MARCADO ocurre varias veces, luego del marcado y antes de la
+        ejecución condicional entre la reasignación y el aligeramiento
+        de condiciones óptimas.
+    Estos pasos de marcado pueden nunca ocurrir.
+    PASO_ASIGNACION_FINAL ocurre luego de obtener la asignación final.
+
+    La función será llamada en cada paso indicado con los argumentos
+    correspondientes; dependiendo del paso, algunos pueden ser nulos.
+    La función recibe los objetos originales, por lo que no debe modificarlos;
+    de lo contrario el programa podría entrar en un estado indefinido.
+    Si la función devuelve True a partir de la asignacion inicial, se detiene
+    el programa y se devuelve las asignaciones 'tal cual'.
+    Si el argumento es None, no se realiza ninguna llamada.
+    Se puede utilizar una función predefinida que muestra información
+    en la salida estándar pasando True como argumento.
+
+    'disponibilidad_uniforme' indica si todas las filas de la matriz deberían
+    tener el mismo tamaño (relevante para una lista de listas).  Si es True
+    y la matriz no cumple las restricciones, se genera una excepción.
+    Si es False, los datos faltantes se rellenan con infinito positivo.
+
+    Si se produce un error de validación, se lanza una excepción ValueError.
+    """
+    if funcion_pasos is True:
+        funcion_pasos = mostrar_pasos
+
     validar_matriz(matriz_costos, disponibilidad_uniforme)
     matriz_costos = procesar_matriz(matriz_costos, disponibilidad_uniforme)
-    print("Procesada:")
-    mostrar_matriz(matriz_costos)
-    print()
+    if funcion_pasos is not None:
+        argumentos_pasos = [matriz_costos, None, None, None, None]
+        funcion_pasos(*argumentos_pasos, PASO_PROCESAR)
     orden = len(matriz_costos)
-    
+
     for fila in matriz_costos:
         minimo = min(fila)
         for i, costo in enumerate(fila):
             fila[i] = costo - minimo
-    print("Reducción por filas:")
-    mostrar_matriz(matriz_costos)
-    print()
+    if funcion_pasos is not None:
+        funcion_pasos(*argumentos_pasos, PASO_REDUCCION_FILAS)
+
     for j in range(orden):
         minimo = min(matriz_costos[i][j] for i in range(orden))
         for i in range(orden):
             matriz_costos[i][j] = matriz_costos[i][j] - minimo
-    print("Reducción por columnas:")
-    mostrar_matriz(matriz_costos)
-    print()
+    if funcion_pasos is not None:
+        funcion_pasos(*argumentos_pasos, PASO_REDUCCION_COLUMNAS)
 
     columnas_marcadas = [False] * orden
     asignaciones = asignar(matriz_costos, columnas_marcadas)
+    if funcion_pasos is not None:
+        argumentos_pasos[4] = asignaciones
+        if funcion_pasos(*argumentos_pasos, PASO_ASIGNACION_INICIAL) == True:
+            return asignaciones
     if len(asignaciones) == orden:
         return asignaciones
-    print("Asignación inicial:", asignaciones, end="\n\n")
 
     filas_marcadas = [False] * orden
     matriz_ceros = [[0] * orden for i in range(orden)]
     for i, j in asignaciones:
         matriz_ceros[i][j] = 1  # Ceros asignados (*)
     falta_reasignar = False
+    if funcion_pasos is not None:
+        argumentos_pasos[1:4] = [matriz_ceros, filas_marcadas, columnas_marcadas]
     while len(asignaciones) < orden:
-        print("Ceros:")
-        mostrar_matriz(matriz_ceros)
-        print("Costos:")
-        mostrar_matriz(matriz_costos)
-        print("Filas marcadas:", filas_marcadas)
-        print("Columnas marcadas:", columnas_marcadas)
-        if input("¿Continuar?: "):
-            return sorted(asignaciones)
-        print()
-        
+        if funcion_pasos is not None:
+            if funcion_pasos(*argumentos_pasos, PASO_INICIO_MARCADO) == True:
+                return sorted(asignaciones)
+
         minimo = INFINITO
         for i in range(orden):
             if filas_marcadas[i]:
@@ -74,15 +134,16 @@ def hungaro(matriz_costos, disponibilidad_uniforme=True):
                 break
             filas_marcadas[i] = True
             columnas_marcadas[j] = False
-        print("Luego de re-marcar:",
-              "Ceros:", sep="\n")
-        mostrar_matriz(matriz_ceros)
-        print("Filas marcadas:", filas_marcadas)
-        print("Columnas marcadas:", columnas_marcadas, end="\n\n")
+        if funcion_pasos is not None:
+            if funcion_pasos(*argumentos_pasos, PASO_FIN_MARCADO) == True:
+                return sorted(asignaciones)
 
         if falta_reasignar:
             asignaciones = reasignar(matriz_ceros, (i, j), columnas_marcadas)
             filas_marcadas = [False] * orden
+            if funcion_pasos is not None:
+                argumentos_pasos[2] = filas_marcadas
+                argumentos_pasos[4] = asignaciones
             falta_reasignar = False
             continue
         for i, fila in enumerate(matriz_costos):
@@ -95,15 +156,9 @@ def hungaro(matriz_costos, disponibilidad_uniforme=True):
                     if not columnas_marcadas[j]:
                         fila[j] -= minimo
 
-    print("Ceros:")
-    mostrar_matriz(matriz_ceros)
-    print("Costos:")
-    mostrar_matriz(matriz_costos)
-    print("Filas marcadas:", filas_marcadas)
-    print("Columnas marcadas:", columnas_marcadas)
-    print("Asignación final:", asignaciones, end="\n\n")
-
     asignaciones.sort()
+    if funcion_pasos is not None:
+        funcion_pasos(*argumentos_pasos, PASO_ASIGNACION_FINAL)
     return asignaciones
 
 def validar_matriz(matriz_costos, disponibilidad_uniforme=True):
@@ -131,7 +186,11 @@ def validar_matriz(matriz_costos, disponibilidad_uniforme=True):
         raise ValueError(ERROR_MATRIZ_VACIA)
 
 def procesar_matriz(matriz_costos, disponibilidad_uniforme=True):
-    matriz_costos = [[float(costo) for costo in fila] for fila in matriz_costos]
+    try:
+        matriz_costos=[[int(costo) for costo in fila] for fila in matriz_costos]
+    except ValueError:
+        matriz_costos = \
+                  [[float(costo) for costo in fila] for fila in matriz_costos]
     cant_filas = len(matriz_costos)
     if disponibilidad_uniforme:
         orden = max(cant_filas, len(matriz_costos[0]))
@@ -235,6 +294,50 @@ def mostrar_matriz(matriz):
     for fila in matriz:
         print("[", *("%5d" % elemento for elemento in fila), "]")
 
+def mostrar_pasos(matriz_costos, matriz_ceros, filas_marcadas,
+                  columnas_marcadas, asignaciones, paso):
+    if paso == PASO_PROCESAR:
+        print("Procesada:")
+        mostrar_matriz(matriz_costos)
+        print()
+    elif paso == PASO_REDUCCION_FILAS:
+        print("Reducción por filas:")
+        mostrar_matriz(matriz_costos)
+        print()
+    elif paso == PASO_REDUCCION_COLUMNAS:
+        print("Reducción por columnas:")
+        mostrar_matriz(matriz_costos)
+        print()
+    elif paso == PASO_ASIGNACION_INICIAL:
+        print("Asignación inicial:", asignaciones, end="\n\n")
+    elif paso == PASO_INICIO_MARCADO:
+        print("Ceros:")
+        mostrar_matriz(matriz_ceros)
+        print("Costos:")
+        mostrar_matriz(matriz_costos)
+        print("Filas marcadas:", filas_marcadas)
+        print("Columnas marcadas:", columnas_marcadas)
+        if input("¿Continuar?: "):
+            return True
+        print()
+    elif paso == PASO_FIN_MARCADO:
+        print("Luego de re-marcar:",
+              "Ceros:", sep="\n")
+        mostrar_matriz(matriz_ceros)
+        print("Filas marcadas:", filas_marcadas)
+        print("Columnas marcadas:", columnas_marcadas, end="\n\n")
+    elif paso == PASO_ASIGNACION_FINAL:
+        print("Ceros:")
+        mostrar_matriz(matriz_ceros)
+        print("Costos:")
+        mostrar_matriz(matriz_costos)
+        print("Filas marcadas:", filas_marcadas)
+        print("Columnas marcadas:", columnas_marcadas)
+        print("Asignación final:", asignaciones, end="\n\n")
+    else:
+        raise NotImplementedError(
+            "Paso no implementado en función de muestra integrada")
+
 
 ### Funcion principal para poder ejecutar el programa
 def main():
@@ -279,4 +382,6 @@ def main():
         ### Salir del programa
         else:
             break
-main()
+
+if __name__ == "__main__":
+    main()
